7af129a85a22e2532f077cdf6b76bbd61e3b9871
Kostis Kapelonis <kostis@codefresh.io>
2022-09-09 16:34:23 +0300

    Simple counter

--- main.go
+++ main.go
@@ -3,11 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
 )
+
+type notificationHandler struct {
+	mu sync.Mutex // guards n
+	n  int
+}
 
 func main() {
 
-	http.HandleFunc("/callme", list_notifications)
+	nf := new(notificationHandler)
+
+	http.HandleFunc("/callme", nf.listNotifications)
+	http.HandleFunc("/notify", nf.createNotification)
+	http.HandleFunc("/clear", nf.clearNotifications)
 
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
@@ -16,10 +26,22 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
-func list_notifications(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "<div class=\"entry\"><span id=\"dfdf\">No ArgoCD Notifications yet</span></div>")
+func (nh *notificationHandler) listNotifications(w http.ResponseWriter, req *http.Request) {
+	nh.mu.Lock()
+	defer nh.mu.Unlock()
+	fmt.Fprintf(w, "<div class=\"entry\"><span id=\"dfdf\">%d</span></div>", nh.n)
 }
 
-// func create_notification(w http.ResponseWriter, req *http.Request) {
-// 	fmt.Fprintf(w, "<div class=\"entry\"><span id=\"dfdf\">No ArgoCD Notifications yet</span></div>")
-// }
+func (nh *notificationHandler) createNotification(w http.ResponseWriter, req *http.Request) {
+	nh.mu.Lock()
+	defer nh.mu.Unlock()
+	nh.n++
+	fmt.Fprintf(w, "Created")
+}
+
+func (nh *notificationHandler) clearNotifications(w http.ResponseWriter, req *http.Request) {
+	nh.mu.Lock()
+	defer nh.mu.Unlock()
+	nh.n = 0
+	fmt.Fprintf(w, "Created")
+}
